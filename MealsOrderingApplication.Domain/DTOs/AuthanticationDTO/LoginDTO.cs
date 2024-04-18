451abd6a2c75b5bac562495b37ac4a3007c1f2f6
ak4m410x01 <abdallah10kamal@gmail.com>
2024-04-18 12:01:49 +0200

    Fix Email Validation Issue

--- MealsOrderingApplication.Domain/DTOs/AuthanticationDTO/LoginDTO.cs
+++ MealsOrderingApplication.Domain/DTOs/AuthanticationDTO/LoginDTO.cs
@@ -5,6 +5,8 @@ namespace MealsOrderingApplication.Domain.DTOs.AuthanticationDTO
     public class LoginDTO
     {
         [Required(ErrorMessage = "The Email field is required.")]
+        [StringLength(256, ErrorMessage = "The Email field must be less than 256 characters.")]
+        [RegularExpression("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", ErrorMessage = "Must be a valid email")]
         public string Email { get; set; } = default!;
 
         [Required(ErrorMessage = "The Password field is required.")]
